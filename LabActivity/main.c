84fdc678a380091e56577ad4079d267cfa5bbd8d
harryhsu123 <49621763+harryhsu123@users.noreply.github.com>
2020-09-07 20:07:33 -0600

    Update main.c

--- LabActivity/main.c
+++ LabActivity/main.c
@@ -4,15 +4,7 @@ int main(void) {
    // Draw tree
    printf("   *\n");
    printf("  ***\n");
-   printf(" *****\n");
-   printf("*******\n");
-   printf("  ***\n");
-   printf("\n\n");
-   printf(" /\\   /\\   \n");
-   printf("   o o   \n");
-   printf("  =   = \n");
-   printf("   ---   ");
-   printf(" \n ");
+
 
 
    /* Type your code here. */
